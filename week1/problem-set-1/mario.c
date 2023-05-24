8c592914019b1f28fe197705586bc66b2cbc20e0
Conor <conor.ganly@yahoo.com>
2023-05-24 02:54:45 +0100

    Add solution to week1/problem-set-1

--- week1/problem-set-1/mario.c
+++ week1/problem-set-1/mario.c
@@ -1,4 +1,47 @@
-// # Here we will be recreating the pyramids from the
-// Super mario image below using the terminal and 
-// print statements.
+#include <stdio.h>
 
+int main(void)
+{
+    // Get number from user
+    int number;
+    printf("Pyramid height (Must be between 1 and 8)\n");
+    while(1)
+    {
+        int result = scanf("%d", &number);
+        // Clear input buffer
+        while (getchar() != '\n');
+
+        if (result == 1 && number > 0 && number <=8)
+        {
+            break;
+        }
+        else
+        {
+            printf("Pyramid height (Must be between 1 and 8)\n");
+        }
+    }
+
+    // Build pyramid
+    for (int i = 1; i <= number; i++)
+    {
+        // Print spaces before each row according to height
+        for (int j = 1; j <= number - i; j++)
+        {
+            printf(" ");
+        }
+        // Print first part of row
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        // Print dividing space
+        printf("  ");
+
+        // Print second part of row
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
