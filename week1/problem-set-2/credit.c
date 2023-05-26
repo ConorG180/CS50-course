4cc6ac545f28cdb6a92a2f472495f6a58a4ac3ea
Conor <conor.ganly@yahoo.com>
2023-05-26 18:34:22 +0100

    Add comments to code and remove print debugging statements

--- week1/problem-set-2/credit.c
+++ week1/problem-set-2/credit.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Function declarations
 long long requestCreditCardNum(void);
 short isValid(long long);
 short getCardNumLength(long long);
@@ -8,15 +9,21 @@ void checkCardType(short firstTwoDigits, short cardNumLength);
 
 int main(void)
 {
+    // Get card number from user
     long long cardNum = requestCreditCardNum();
+
+    // If card is not valid, print "INVALID" and exit
     if (isValid(cardNum) != 1)
     {
         printf("INVALID");
         return 1;
     }
+
+    // Get card number length and first two digits
     short cardNumLength = getCardNumLength(cardNum);
     short firstTwoDigits = getFirstTwoDigits(cardNum);
-    printf("First 2 digits: %i", firstTwoDigits);
+
+    // Check the card type and print accordingly
     checkCardType(firstTwoDigits, cardNumLength);
     return 1;
 }
@@ -47,8 +54,8 @@ short isValid(long long cardNum) {
         cardNum /= 10;
     }
 
+    // If the total sum ends in 0, the card is valid
     int totalSum = sum1 + sum2;
-    printf("Sum1: %d, Sum2: %d, Total Sum: %d\n", sum1, sum2, totalSum);
     if (totalSum % 10 == 0)
     {
         return 1;
@@ -62,12 +69,14 @@ short isValid(long long cardNum) {
 
 long long requestCreditCardNum(void)
 {
-        long long cardNum;
+    long long cardNum;
+    // Keep asking for a number until a valid card number is entered
     while(1)
     {
         printf("Credit card Number:");
         int result = scanf("%lld", &cardNum);
 
+        // Check if entered card number is of valid length
         long long tempCardNum = cardNum;
         short cardNumLength = getCardNumLength(tempCardNum);
         while(getchar() != '\n');
@@ -83,6 +92,7 @@ long long requestCreditCardNum(void)
 short getCardNumLength(long long cardNum)
 {
     short cardNumLength = 0;
+    // Count number of digits in card number
     while (cardNum != 0)
     {
         cardNum /= 10;
@@ -93,30 +103,33 @@ short getCardNumLength(long long cardNum)
 
 short getFirstTwoDigits(long long cardNum)
 {
-    printf("cardNum: %lld", cardNum);
     long long firstTwoDigits = cardNum;
+    // Reduce card number until only first two digits remain
     while(firstTwoDigits > 100)
     {
         firstTwoDigits /= 10;
     } 
-    printf("First 2 digits: %i", firstTwoDigits);
     return (short) firstTwoDigits;
 }
 
 void checkCardType(short firstTwoDigits, short cardNumLength)
 {
+    // Check for American Express
     if ((firstTwoDigits == 34 || firstTwoDigits == 37) && cardNumLength == 15)
     {
         printf("AMEX\n");
     }
+    // Check for Mastercard
     else if (firstTwoDigits >= 51 && firstTwoDigits <= 55 && cardNumLength == 16)
     {
         printf("MASTERCARD\n");
     }
+    // Check for Visa
     else if (firstTwoDigits / 10 == 4 && cardNumLength == 13 || cardNumLength == 16)
     {
         printf("VISA\n");
     }
+    // If none of the above, print "UNKNOWN CARD BRAND"
     else
     {
         printf("UNKNOWN CARD BRAND");
