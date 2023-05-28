b06772b4a361d52122fcd82d918074c331ef38bb
Conor <conor.ganly@yahoo.com>
2023-05-28 23:14:33 +0100

    Create function to check length and alpha chars in cipher

--- week2/problem-set-1/substitution.c
+++ week2/problem-set-1/substitution.c
@@ -3,10 +3,16 @@
 
 // Function prototypes:
 int checkArguments(short numberOfArguments);
+int checkCipher(char cipher[]);
 
 int main(int argc, char *argv[])
 {
     if(checkArguments(argc) == 1)
+    {
+        return 1;
+    }
+
+    if(checkCipher(argv[1]) == 1)
     {
         return 1;
     }
@@ -20,4 +26,23 @@ int checkArguments(short numberOfArguments)
         return 1;
     }
     return 0;
+}
+
+int checkCipher(char cipher[])
+{
+    short cipherLength = strlen(cipher);
+    if (cipherLength != 26)
+    {
+        printf("Error - String not 26 characters");
+        return 1;
+    }
+
+    for (int i=0; i < cipherLength; i++)
+    {
+        if (cipher[i] < 65 || cipher[i] > 122)
+        {
+            printf("Error - String contains non-alphabetic characters");
+            return 1;
+        }
+    }
 }
